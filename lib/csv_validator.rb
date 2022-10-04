374b9a490b0a177cc37aacd7b509f4b9ef0d38be
Shawn Kuo <were1122was@gmail.com>
2022-10-04 17:53:20 +0800

    Rspec all pass

--- lib/csv_validator.rb
+++ lib/csv_validator.rb
@@ -9,6 +9,7 @@ class CsvValidator
     @file_path = file_path
     @table_info = table_info
     @csv = CSV.table(file_path, { header_converters: lambda { |header| header.to_s } })
+    @header = @csv.headers
     @errors = []
     # TODO, add any initialize process if you need
   end
@@ -17,9 +18,14 @@ class CsvValidator
     # TODO
     p "=" * 50 + "file" + "=" * 50
     p @file_path
+
+    # 判斷空白檔案
+    @errors.push("Empty Content") if @csv.empty?
+
     @csv.each do |row|
       id = row[0]
       row.each do |key|
+        # 判斷時間
         if @table_info.timestamp_columns.include?(key[0])
           begin
             myDate = DateTime.strptime(key[1], "%Y-%m-%d %H:%M:%S")
@@ -27,40 +33,32 @@ class CsvValidator
             @errors.push("Time Format Violation at #{key[0]} in Row ID=#{id}")
           end
         end
+        # 判斷長度
+        @table_info.length_limit_data(@header).each do |n|
+          if n[0].include?(key[0])
+            if key[1].length > n[1]
+              @errors.push("Length Limit Violation at #{n[0]}(#{n[1]}) in Row ID=#{id}")
+            end
+          end
+        end
       end
-    end
-    # p "-" * 50 + "row[0]" + "-" * 50
-    # p @csv.by_row[0]
-    # p "=" * 50 + "row[1]" + "=" * 50
-    # p @csv.by_row[1]
-    if  @csv.by_row[0] == nil
-      @errors.push('Empty Content')
-      return false
     end
 
     if  @csv.by_col[0].uniq! != nil
       id_column = @csv.by_col[0]
       id = id_column.detect{ |e| id_column.count(e) > 1 }
       @errors.push("Duplicate Ids: [#{id}]")
-      return false 
     end
 
-    
     if  @csv.by_col[5].include? (nil)
       @errors.push('Not Null Violation at character_id in Row ID=1')
     end
 
-    # p "~" * 50
-    # @csv.by_col[6]
-    # a = @csv.by_col[6].map { |string| string.split(' ') }
-    # i = 0
-    # while i < a.length
-    #   puts myDate = Date.parse(a[i][0]) rescue nil
-    #   i += 1
-    # end
-
-    p 
-    true
+    if @errors.present?
+      false
+    else
+      true
+    end
   end
 
   private
